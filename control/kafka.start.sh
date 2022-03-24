772be5bd25807cef5f6bba73263d20ce66eabfd1
Denis Rystsov <denis@vectorized.io>
2022-03-24 09:34:55 -0700

    update kafka version

--- control/kafka.start.sh
+++ control/kafka.start.sh
@@ -4,5 +4,5 @@ set -e
 
 export KAFKA_HEAP_OPTS="-Xms16g -Xmx16g -XX:MetaspaceSize=96m"
 export KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+ParallelRefProcEnabled -XX:+AggressiveOpts -XX:+DoEscapeAnalysis -XX:ParallelGCThreads=12 -XX:ConcGCThreads=12 -XX:+DisableExplicitGC -XX:-ResizePLAB -XX:MinMetaspaceFreeRatio=50 -XX:MaxMetaspaceFreeRatio=80 -Djava.awt.headless=true"
-export KAFKA=/mnt/vectorized/bin/kafka_2.13-3.0.0
+export KAFKA=/mnt/vectorized/bin/kafka_2.13-3.0.1
 nohup $KAFKA/bin/kafka-server-start.sh /mnt/vectorized/kafka.properties > /mnt/vectorized/kafka/log.kafka.$(date +%s) 2>&1 & echo $! > /mnt/vectorized/kafka/pid
