191a79ddeaee9a59ec6c2f3700d38a427b12318a
Denis Rystsov <denis@vectorized.io>
2023-04-07 18:01:04 -0700

    wip

--- playbooks/test.all.sh
+++ playbooks/test.all.sh
@@ -11,7 +11,7 @@ if [ ! -f id_ed25519 ]; then
     ssh-keygen -t ed25519 -f id_ed25519 -N ""
 fi
 
-terraform apply -var="username=$AWS_CHAOS_RESOURCE_PREFIX" -var="redpanda_cluster_size=3" -var="workload_cluster_size=2" -auto-approve
+terraform apply -var="username=$AWS_CHAOS_RESOURCE_PREFIX" -var="redpanda_cluster_size=6" -var="workload_cluster_size=2" -auto-approve
 sleep 1m
 
 control=$(ansible-inventory -i hosts.ini --list | jq -r ".control.hosts[0]")
@@ -26,5 +26,5 @@ ansible-playbook playbooks/control.execute.tests.yml --key-file id_ed25519
 ssh -i ./id_ed25519 ubuntu@$control tar -czf /mnt/vectorized/txperf/results.tar.gz -C /mnt/vectorized/txperf results
 scp -i ./id_ed25519 -r ubuntu@$control:/mnt/vectorized/txperf/results.tar.gz results.tar.gz
 
-terraform destroy -var="username=$AWS_CHAOS_RESOURCE_PREFIX" -var="redpanda_cluster_size=3" -var="workload_cluster_size=2" -auto-approve
+terraform destroy -var="username=$AWS_CHAOS_RESOURCE_PREFIX" -var="redpanda_cluster_size=6" -var="workload_cluster_size=2" -auto-approve
 
